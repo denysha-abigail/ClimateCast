e70f7f820fe77d575dfb858f88fb80e17bfd00fb
Denysha Abigail <96617237+denysha-abigail@users.noreply.github.com>
2022-04-01 01:27:47 -0400

    hardcoded html, appended current forecast, and added css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,12 @@
+var currentContainer = document.getElementById("current-data");
+var cityTitle = document.getElementById("city-title");
+
+const date = new Date();
+const month = date.getMonth()+1;
+const day = date.getDate();
+const year = date.getFullYear();
+const fullYear = " " + "(" + month + "/" + day + "/" + year + ")";
+
 document.querySelector('button').addEventListener('click', handleClick);
 
 function handleClick() {
@@ -7,11 +16,32 @@ function handleClick() {
         fetch(url)
             .then(data => data.json())
             .then(data => {
-                const { lat, lon } = data[0];
+                const { lat, lon, name } = data[0];
+                cityTitle.innerHTML = 
+                    `<h3 class="current-city">${name + fullYear}</h3>`
                 let url2 = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${api_key}`
                 fetch(url2)
                     .then(data => data.json())
-                    .then(console.log);
-            })
+                    .then(data => {
+                        const { temp, wind_speed, humidity, uvi } = data.current;
+                        currentContainer.innerHTML =
+                            `<div class="current-data">
+                                <div>Temp: ${temp}°F</div>
+                            </div>
+                            <div class="current-data">
+                                <div>Wind: ${wind_speed}</div>
+                            </div>
+                            <div class="current-data">
+                                <div>Humidity: ${humidity}%</div>
+                            </div>
+                            <div class="current-data">
+                                <div>UV Index: ${uvi}</div>
+                            </div>`;
+
+                            
+                    }
+            )})
+    } else if (!city) {
+        alert("Please enter a city.");
     }
-}
+};
