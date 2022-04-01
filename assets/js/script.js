e57eeeb0c13caffe610edac4ac7c580819aef386
Denysha Abigail <96617237+denysha-abigail@users.noreply.github.com>
2022-04-01 18:24:05 -0400

    restructured js file with more functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,30 +13,38 @@ document.querySelector('button').addEventListener('click', handleClick);
 function handleClick() {
     let city = document.getElementById('city').value.trim();
     if (city) {
-        let url = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${api_key}`
-        fetch(url)
-            .then(data => data.json())
-            .then(data => {
-                const { lat, lon, name } = data[0];
-                cityTitle.innerHTML =
-                    `<h3 class="current-city">${name + fullYear}</h3>`
-                let url2 = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${api_key}`
-                fetch(url2)
-                    .then(data => data.json())
-                    .then(data => {
-                        const { temp, wind_speed, humidity, uvi } = data.current;
-                        const { icon } = data.current.weather[0];
+        fetchData(city);
+    } else if (!city) {
+        alert("Please enter a city.");
+    }
+};
 
-                            var colorBlock = ""
-                                if (uvi > 7) {
-                                    colorBlock = "severe";
-                                } else if (uvi > 4 && uvi < 7) {
-                                    colorBlock = "moderate";
-                                } else if (uvi < 4) {
-                                    colorBlock = "favorable";
-                                }
 
-                        currentContainer.innerHTML =
+function fetchData(city) {
+    let url = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${api_key}`
+    fetch(url)
+        .then(data => data.json())
+        .then(data => {
+            const { lat, lon, name } = data[0];
+            cityTitle.innerHTML =
+                `<h3 class="current-city">${name + fullYear}</h3>`
+            let url2 = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${api_key}`
+            fetch(url2)
+                .then(data => data.json())
+                .then(data => {
+                    const { temp, wind_speed, humidity, uvi } = data.current;
+                    const { icon } = data.current.weather[0];
+
+                    var colorBlock = ""
+                    if (uvi > 7) {
+                        colorBlock = "severe";
+                    } else if (uvi > 4 && uvi < 7) {
+                        colorBlock = "moderate";
+                    } else if (uvi < 4) {
+                        colorBlock = "favorable";
+                    }
+
+                    currentContainer.innerHTML =
                             `<img src="http://openweathermap.org/img/w/${icon}.png">
                             <div class="current-data">
                                 <div>Temp: ${temp}°F</div>
@@ -45,18 +53,18 @@ function handleClick() {
                                 <div class="color ${colorBlock}">UV Index: ${uvi}</div>
                             </div>`;
 
-                        for (var i = 1; i <= 5; i++) {
-                            var weatherData = {
-                                date: data.daily[i].dt,
-                                temp: data.daily[i].temp.day,
-                                wind_speed: data.daily[i].wind_speed,
-                                humidity: data.daily[i].humidity,
-                                icon: data.current.weather[0].icon
-                            };
+                    for (var i = 1; i <= 5; i++) {
+                        var weatherData = {
+                            date: data.daily[i].dt,
+                            temp: data.daily[i].temp.day,
+                            wind_speed: data.daily[i].wind_speed,
+                            humidity: data.daily[i].humidity,
+                            icon: data.current.weather[0].icon
+                        };
 
-                            var currentDate = moment.unix(weatherData.date).format("MM/DD/YYYY");
+                        var currentDate = moment.unix(weatherData.date).format("MM/DD/YYYY");
 
-                            document.getElementById("forecast-data-" + i).innerHTML =
+                        document.getElementById("forecast-data-" + i).innerHTML =
                                 `<div>
                                     <h4>${currentDate}</h4>
                                     <img src="http://openweathermap.org/img/w/${weatherData.icon}.png">
@@ -64,11 +72,7 @@ function handleClick() {
                                     <div>Wind: ${weatherData.wind_speed}mph</div>
                                     <div">Humidity: ${weatherData.humidity}%</div>
                                 </div>`;
-                        }
                     }
-                )
-            })
-    } else if (!city) {
-        alert("Please enter a city.");
-    }
+                })
+        })
 };
